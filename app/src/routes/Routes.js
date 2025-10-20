6c13b8002a068bf893e40f8383dd4efb41ebf951
Arthur Pereira <arthurpsouza502@gmail.com>
2025-10-20 18:24:30 -0300

    routes funcionando
    agora o server.js abre pelo o menos

--- app/src/routes/Routes.js
+++ app/src/routes/Routes.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('../controllers/Controller');
 
-router.get('/tarefas', controllers.getTarefas);
-router.get('/tarefasFeitas/', controllers.getTarefasFeitas );
-router.post('/tarefas/', controllers.criarTarefa );
+
 router.get('/', controllers.paginaInicial);
-router.delete('/tarefas/', controllers.deletarTarefa);
-router.put('/tarefas/', controllers.editarTarefa);
+
 module.exports = router;
