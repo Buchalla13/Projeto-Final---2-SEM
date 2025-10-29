c12b2695994e700fc1f47591d088b123fff67378
Leonardo Aleixo <leonardo.aleixo@aluno.ifsp.edu.br>
2025-10-29 16:33:36 -0300

    avançando no view
    apenas criei rotas e iniciei algo para o cadastro e login dos users

--- app/src/routes/Routes.js
+++ app/src/routes/Routes.js
@@ -2,7 +2,18 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('../controllers/Controller');
 
-
 router.get('/', controllers.paginaInicial);
+
+router.get('/inscrever', (req, res) => {
+  res.send('a rota pra novo cliente ta rodando'); // todos os res.send são pra teste
+});
+
+router.get('/login', (req, res) => {
+  res.send('a rota logar ta rodando');
+});
+
+router.get('/trabalhe_conosco', (req, res) => {
+  res.send('a rota do CLT ta rodando');
+});
 
 module.exports = router;
