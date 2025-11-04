d8f6caeeb4371c19b1dd4fac1ca97bc6c53a40d6
Arthur Araujo de Oliveira <gui2009prado@gmail.com>
2025-11-04 12:27:12 -0300

    arrumei a rota

--- app/src/routes/Routes.js
+++ app/src/routes/Routes.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controllers = require('../controllers/Controller');
+const controllers = require("../controllers/Controller");
 
-router.get('/', controllers.paginaInicial);
+router.get("/", controllers.paginaInicial);
 
-router.get('/inscrever', (req, res) => {
+router.get("/inscrever", (req, res) => {
   // renderiza a view cadastro.ejs (views path configurado em app.js)
-  res.render('cadastro');
+  res.render("cadastro_cliente");
 });
 
-router.get('/login', (req, res) => {
-  res.render('login');
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
-router.get('/trabalhe_conosco', (req, res) => {
-  res.render('funcionario');
+router.get("/trabalhe_conosco", (req, res) => {
+  res.render("funcionario");
 });
 
 module.exports = router;
