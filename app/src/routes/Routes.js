c2178fab3b54f89ce942a3825db2a10f1b06673a
Kaysheen <gui2009prado@gmail.com>
2025-11-18 14:05:06 -0300

    resumindo o view e includes

--- app/src/routes/Routes.js
+++ app/src/routes/Routes.js
@@ -8,10 +8,14 @@ router.get("/", controllers.paginaInicial);
 router.get("/inscrever", (req, res) => res.render("cadastro_cliente"));
 router.get("/inscrever_funcionario", (req, res) => res.render("cadastro_funcionario"));
 router.get("/login_cliente", (req, res) => res.render("login_cliente"));
-router.get("/login_funcionario", (req, res) => res.render("login_funcionario"));
+// agora a rota de login do funcionário usa a view `funcionario.ejs` com campo de código
+router.get("/login_funcionario", (req, res) => res.render("funcionario"));
 router.get("/trabalhe_conosco", (req, res) => res.render("funcionario"));
 router.get("/sobre", (req, res) => res.render("sobre"));
 
 router.post("/inscrever",controllerU.inscrever)
+// rotas de autenticação
+router.post('/login_cliente', controllerU.LoginCliente);
+router.post('/login_funcionario', controllerU.loginFuncionario);
 
 module.exports = router;
